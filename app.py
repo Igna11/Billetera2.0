4e63f27243fa0ab57fc8606c216a7f581426014e
igna <ignaao@gmail.com>
2025-07-18 16:03:59 -0300

    icons
    agregados setApplicationName y setApplicationDisplayName para que se vea el ícono de la app en el dock de ubuntu

--- app.py
+++ app.py
@@ -2,15 +2,26 @@ import os
 import sys
 
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QStackedWidget
+from PyQt5.QtWidgets import QApplication, QStackedWidget, QMainWindow
 
 from billeUI import welcomescreen, ICONSPATH
 
+class BilleterApp(QMainWindow):
+    def __init__(self, stacked_widget):
+        super().__init__()
+        self.setWindowTitle("BilleterApp")
+        self.setCentralWidget(stacked_widget)
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon(os.path.join(ICONSPATH, "wallet.png")))
+    app.setWindowIcon(QIcon(os.path.join(ICONSPATH, "wallet2.png")))
+    app.setApplicationName("BilleterApp")
+    app.setApplicationDisplayName("BilleterApp")
     widget = QStackedWidget()
     main_window = welcomescreen.WelcomeScreen(widget=widget)
     widget.addWidget(main_window)
-    widget.show()
+
+    window = BilleterApp(widget)
+    window.show()
     sys.exit(app.exec())
