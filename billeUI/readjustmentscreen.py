e013b406f655a259990a5eb5ac5d83695cb98f72
igna <ignaao@gmail.com>
2025-07-21 21:18:38 -0300

    fix and feature
    fix de un bug que si la cuenta tenia 0 o none tiraba error en el readjustment. Y agregado de label animada

--- billeUI/readjustmentscreen.py
+++ billeUI/readjustmentscreen.py
@@ -14,8 +14,7 @@ from PyQt5.QtWidgets import QMainWindow
 from src.queries.accqueries import ListAccountsQuery
 from src.ophandlers.operationhandler import OperationHandler
 
-from billeUI import UISPATH
-from billeUI import operationscreen
+from billeUI import UISPATH, operationscreen, animatedlabel
 
 
 class ReadjustmentScreen(QMainWindow):
@@ -27,26 +26,18 @@ class ReadjustmentScreen(QMainWindow):
         super(ReadjustmentScreen, self).__init__(parent)
         operation_readjustment_screen = os.path.join(UISPATH, "operation_readjustment_screen.ui")
         loadUi(operation_readjustment_screen, self)
-        self.readjustment_stacked_widget
+        # self.readjustment_stacked_widget -> main widget
         self.widget = widget
         self.acc_name = None
         self.acc_currency = None
-        self.operation_flag = operation_flag
         self.set_account_info()
         self.accounts_comboBox.addItems(self.acc_names_list)
-        self.set_acc_data(self.accounts_comboBox.currentIndex())
-        self.accounts_comboBox.currentIndexChanged.connect(self.set_acc_data)
+        self.set_account_data(self.accounts_comboBox.currentIndex())
+        self.accounts_comboBox.currentIndexChanged.connect(self.set_account_data)
         self.save_button.clicked.connect(self.save)
         self.cancel_button.clicked.connect(self.cancel)
         self.more_radio_button.clicked.connect(self.more_button)
         self._reset_more_data()
-
-    def set_account_info(self) -> None:
-        """Sets the account objects, names y currencies"""
-        acc_object_list = ListAccountsQuery(user_id=self.widget.user_object.user_id).execute()
-        self.acc_object_list = [acc for acc in acc_object_list if acc.account_total]
-        self.acc_currency = [acc.account_currency for acc in self.acc_object_list]
-        self.acc_names_list = [f"{acc.account_name} ({acc.account_currency})" for acc in self.acc_object_list]
 
     def _reset_more_data(self) -> None:
         """Reset the data from the toggle more"""
@@ -67,11 +58,17 @@ class ReadjustmentScreen(QMainWindow):
             self.quantity_line.setText("")
             self.status_label.setText("")
 
-    def set_acc_data(self, i: int):
-        """Sets the values of acc_name, acc_currency and the value of total label."""
-        self.acc_object_list = [
-            acc for acc in ListAccountsQuery(user_id=self.widget.user_object.user_id).execute() if acc.account_total
-        ]
+    def set_account_info(self, i: int) -> None:
+        """Sets the account objects, names y currencies to be used in the acounts_comboBox widget"""
+        acc_object_list = ListAccountsQuery(user_id=self.widget.user_object.user_id).execute()
+        self.acc_object_list = [acc for acc in acc_object_list]
+        self.acc_currency = [acc.account_currency for acc in self.acc_object_list]
+        self.acc_names_list = [f"{acc.account_name} ({acc.account_currency})" for acc in self.acc_object_list]
+
+    def set_account_data(self, i: int):
+        """Sets the values of acc_name, acc_currency and the value of total label and the
+        account_id to be used when saving the data."""
+        self.acc_object_list = [acc for acc in ListAccountsQuery(user_id=self.widget.user_object.user_id).execute()]
         self.acc_name = self.acc_names_list[i]
         self.account_id = self.acc_object_list[i].account_id
         account_total = self.acc_object_list[i].account_total
@@ -79,41 +76,44 @@ class ReadjustmentScreen(QMainWindow):
 
     def save(self):
         """Saves the new total value of the account."""
-        if self.operation_flag == "readjustment":
-            if self.quantity_line_2.text() != "":
-                value = decimal.Decimal(self.quantity_line_2.text())
-                self.more_data = {
-                    "category": self.category_line.text(),
-                    "subcategory": self.subcategory_line.text(),
-                    "description": self.description_line.text(),
-                }
-            elif self.quantity_line.text() != "":
-                self.more_data = {
-                    "category": "Readjustment",
-                    "subcategory": "Readjustment",
-                    "Description": "Readjustment",
-                }
-                value = decimal.Decimal(self.quantity_line.text())
-            try:
-                readjustment = OperationHandler(
-                    user_id=self.widget.user_object.user_id,
-                    account_id=self.account_id,
-                    operation_type="income",  # dummy operation type, it gets overwritten
-                    amount=0.1,  # dummy value, it gets overwritten
-                    **self.more_data,
-                )
-                cml = readjustment.readjustment(account_total=value)
-                readjustment.create_operations(cml)
-                self.status_label.setText("<font color='green'>Operation successfull</font>")
-            except ValueError as e:
-                self.status_label.setText("<font color='red'>Invalid value.</font>")
-            except decimal.InvalidOperation as e:
-                self.status_label.setText("<font color='red'>Invalid value.</font>")
-            except UnboundLocalError:
-                self.status_label.setText("<font color='red'>No value given to readjust!</font>")
+        if self.quantity_line_2.text() != "":
+            value = decimal.Decimal(self.quantity_line_2.text())
+            self.more_data = {
+                "category": self.category_line.text(),
+                "subcategory": self.subcategory_line.text(),
+                "description": self.description_line.text(),
+            }
+        elif self.quantity_line.text() != "":
+            self.more_data = {
+                "category": "Readjustment",
+                "subcategory": "Readjustment",
+                "Description": "Readjustment",
+            }
+            value = decimal.Decimal(self.quantity_line.text())
+        try:
+            readjustment = OperationHandler(
+                user_id=self.widget.user_object.user_id,
+                account_id=self.account_id,
+                operation_type="income",  # dummy operation type, it gets overwritten
+                amount=0.1,  # dummy value, it gets overwritten
+                **self.more_data,
+            )
+            cml = readjustment.readjustment(account_total=value)
+            readjustment.create_operations(cml)
+            animatedlabel.AnimatedLabel("Operation successfull ✅").display()
+            self.status_label.setText("<font color='green'>Operation successfull</font>")
+        except ValueError as e:
+            animatedlabel.AnimatedLabel("Invalid value!", message_type="error").display()
+            self.status_label.setText("<font color='red'>Invalid value.</font>")
+        except decimal.InvalidOperation as e:
+            animatedlabel.AnimatedLabel("Invalid value!", message_type="error").display()
+            self.status_label.setText("<font color='red'>Invalid value.</font>")
+        except UnboundLocalError:
+            animatedlabel.AnimatedLabel("No value given!", message_type="warning").display()
+            self.status_label.setText("<font color='red'>No value given to readjust!</font>")
 
         # Updates the total value of the account in the label "total_label"
-        self.set_acc_data(self.accounts_comboBox.currentIndex())
+        self.set_account_data(self.accounts_comboBox.currentIndex())
 
     def cancel(self):
         """Returns to the OperationScreen menu"""
