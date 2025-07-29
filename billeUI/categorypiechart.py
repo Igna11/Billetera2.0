fcb2f2bee7c1f8d1755d66d6b0b8cff3fc52360f
igna <ignaao@gmail.com>
2025-07-28 22:42:31 -0300

    WIP
    no anda esto, falta corregir. Por ahora borrando cosas

--- billeUI/categorypiechart.py
+++ billeUI/categorypiechart.py
@@ -26,9 +26,6 @@ class CategoricalPieChart(QtChart.QChart):
     def __init__(self, parent=None) -> None:
         super(CategoricalPieChart, self).__init__(parent)
 
-        self.month = datetime.now().month
-        self.year = datetime.now().year
-
         self.series_outer = QtChart.QPieSeries()
         self.series_inner = QtChart.QPieSeries()
 
@@ -43,69 +40,6 @@ class CategoricalPieChart(QtChart.QChart):
         self.setAnimationOptions(QtChart.QChart.SeriesAnimations)
 
         self.setBackgroundRoundness(20)
-
-    def get_month_interval(self, year: int, month: int) -> Tuple[datetime, datetime]:
-        from_datetime = datetime(year, month, 1, tzinfo=UTC)
-        if month == 12:
-            to_datetime = datetime(year + 1, 1, 1, tzinfo=UTC)
-        else:
-            to_datetime = datetime(year, month + 1, 1, tzinfo=UTC)
-        return from_datetime, to_datetime
-
-    def load_data(
-        self,
-        user_id: str,
-        currency: str,
-        chart_mode: str,
-        time_period: datetime,
-        chart_type: str = "expense",
-    ) -> tuple:
-        """
-        Loads the raw data in the given currency and for the given filters
-        in order to create the desired piechart
-        """
-        if chart_mode == "month":
-            # set the time frame
-            from_datetime, to_datetime = self.get_month_interval(time_period.year, time_period.month)
-
-            data_outer = AccountDataAnalyzer.group_operations(
-                user_id=user_id,
-                from_datetime=from_datetime,
-                to_datetime=to_datetime,
-                operation_type=chart_type,
-                data_type="category",
-                currency=currency,
-            )
-            data_inner = AccountDataAnalyzer.group_operations(
-                user_id=user_id,
-                from_datetime=from_datetime,
-                to_datetime=to_datetime,
-                operation_type=chart_type,
-                data_type="subcategory",
-                currency=currency,
-            )
-
-        elif chart_mode == "period":
-            ci_date, cf_date = time_period.values()
-            data_outer = AccountDataAnalyzer.group_operations(
-                user_id=user_id,
-                from_datetime=ci_date,
-                to_datetime=cf_date,
-                operation_type=chart_type,
-                data_type="category",
-                currency=currency,
-            )
-            data_inner = AccountDataAnalyzer.group_operations(
-                user_id=user_id,
-                from_datetime=ci_date,
-                to_datetime=cf_date,
-                operation_type=chart_type,
-                data_type="subcategory",
-                currency=currency,
-            )
-        else:
-            raise ValueError("Valid types: 'expense', 'income'. Valid modes: 'month', 'period'")
-        return data_inner, data_outer
 
     def clear_slices(self):
         """
@@ -188,49 +122,6 @@ class CategoricalPieChart(QtChart.QChart):
             elif pie_slice.percentage() <= 0.05:
                 pie_slice.hovered.connect(lambda is_hovered, slice_=pie_slice: slice_.setLabelVisible(is_hovered))
             pie_slice.setLabel(label)
-
-    def update_title(
-        self,
-        user_id: str,
-        chart_mode: str,
-        time_period: datetime,
-        currency: str = "ARS",
-        chart_type: str = "expense",
-        ci_date=None,
-        cf_date=None,
-    ) -> str:
-        """
-        Creates the format for the title of the chart and updates it every time a chart
-        is instantiated.
-        chart_mode: monthly or period
-        chart_type: incomes or expenses
-        title_type
-        """
-        if chart_mode == "month":
-            from_datetime, to_datetime = self.get_month_interval(time_period.year, time_period.month)
-            selected_period = time_period.strftime(format="%B %Y").capitalize()
-            total = AccountDataAnalyzer.get_user_totals_by_period(
-                user_id=user_id,
-                from_datetime=from_datetime,
-                to_datetime=to_datetime,
-                operation_type=chart_type,
-                currency=currency,
-            )
-        elif chart_mode == "period":
-            ci_date, cf_date = time_period.values()
-            selected_period = f"Period: {ci_date} -- {cf_date}"
-            total = AccountDataAnalyzer.get_user_totals_by_period(
-                user_id=user_id,
-                from_datetime=ci_date,
-                to_datetime=cf_date,
-                operation_type=chart_type,
-                currency=currency,
-            )
-        title_type = chart_type.capitalize()
-        total_int = f"{total:,.0f}".replace(",", ".")
-        total_decimal = f"{total:.2f}".split(".")[1]
-        title = f"<h3><p align='center' style='color:black'><b>{title_type}: ${total_int}<sup>{total_decimal}</sup><br>{selected_period}</b></p>"
-        self.setTitle(title)
 
     def lighten_color(self, color: QColor, factor: float) -> QColor:
         """
