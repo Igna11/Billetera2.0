cde916b78a7fdd2ba36c10dccd5ed5e81663a1b1
igna <ignaao@gmail.com>
2026-09-01 20:28:53 -0300

    fix
    bug fix que a veces te borraba el nombre de la cuenta al editarla

--- billeUI/accountbrowser.py
+++ billeUI/accountbrowser.py
@@ -349,6 +349,8 @@ class AccountBrowser(QMainWindow):
 
                 animatedlabel.AnimatedLabel("Changes saved! ✅", message_type="success").display()
                 self.save_changes_button.setEnabled(False)
+                # reset self.account_changed => bug fix
+                self.account_changed = set()
             except sqlite3.IntegrityError:
                 animatedlabel.AnimatedLabel("Duplicated name + currency!", message_type="error").display()
             except sqlite3.OperationalError:
